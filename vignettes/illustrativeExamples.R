9e78f7678907e564ddbb80a008ad1f45cd070aea
gilesjohnr <gilesjohnr@gmail.com>
2020-07-21 14:23:22 -0400

    added vignette docs to pkg and website to github

--- vignettes/illustrativeExamples.R
+++
@@ -1,6 +0,0 @@
-## ----setup, include=FALSE-----------------------------------------------------
-knitr::opts_knit$set(root.dir="../")
-knitr::opts_chunk$set(fig.width=7.5, fig.height=6, fig.path='../figures/', warning=FALSE, message=FALSE,cache=FALSE)
-
-library(phylosamp)
-
