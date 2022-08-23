12a52bc67bb07f2d030f6d11056873983db0bb55
eclee25 <eclee25@gmail.com>
2022-08-23 16:44:11 -0700

    unit test calc_samplesize_detect_cont
    some tests don't pass due to known issues in the ms text

--- R/calc_samplesize_detect_cont.R
+++ R/calc_samplesize_detect_cont.R
@@ -17,6 +17,13 @@
 
 
 calc_samplesize_detect_cont <- function(prob,t,p0,r,c_ratio=1){
+
+  if(!all(is.numeric(prob), prob>0 & prob<1)) stop('Desired probability of detection must be numeric and between 0 and 1.')
+    if(!is.numeric(t)) stop('Time step must be numeric.')
+  if(!all(is.numeric(p0), p0>0 & p0<1)) stop('Initial variant prevalence must be numeric and between 0 and 1.')
+  if(!all(is.numeric(r), r!=0)) stop('Growth rate must be numeric and non-zero.')
+  if(!all(is.numeric(c_ratio), c_ratio!=0)) stop('Coefficient of detection ratio must be numeric and non-zero')
+
   n = -(log(1-prob)) / (calc_cdf_logistic(t,p0,r,c_ratio)-calc_cdf_logistic(0,p0,r,c_ratio))
   return(n)
 }
