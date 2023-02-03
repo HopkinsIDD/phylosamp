3bc67c10ead9bd388df603a9cfc13992502d4bb5
Elizabeth Lee <eclee25@gmail.com>
2023-02-03 12:14:51 -0800

    tidying inline args deprecate rd 4

---
+++ R/true_pairs_mtsl.R
@@ -0,0 +1,44 @@
+##' Expected number of true transmission pairs assuming multiple-transmission and single-linkage
+##'
+##' This function calculates the expected number true transmission pairs in a sample of size \code{M}.
+##' The multiple-transmission and single-linkage method assumes the following:
+##' \enumerate{
+##'      \item Each case \eqn{i} is, on average, the infector of \code{R} cases in the population (\eqn{N})
+##'      \item Each case \eqn{i} is allowed to be linked by the linkage criteria to only one other case \eqn{j} in the sampled population (\eqn{M}).
+##'      }
+##'
+##' @param eta scalar or vector giving the sensitivity of the linkage criteria
+##' @param rho scalar or vector giving the proportion of the final outbreak size that is sampled
+##' @param M scalar or vector giving the number of cases sampled
+##' @param R scalar or vector giving the effective reproductive number of the pathogen
+##'
+##' @return scalar or vector giving the expected number of true transmission pairs in the sample
+##'
+##' @author John Giles, Shirlee Wohl and Justin Lessler
+##'
+##' @examples
+##' true_pairs_mtsl(eta=0.95, rho=0.2, M=200, R=1)
+##'
+##' @family true_pairs
+##'
+##' @export
+##'
+##'
+
+true_pairs_mtsl <- function(eta, rho, M, R) {
+    lifecyle::deprecate_soft("1.0.0", "true_pairs_mtsl()", "translink_expected_links_true_mtsl()")
+    lifecyle::deprecate_soft("1.0.0", "true_pairs_mtsl(eta)", "translink_expected_links_true_mtsl(sensitivity)")
+
+    if (!all(is.numeric(eta), eta >= 0 & eta <= 1))
+        stop("eta must be numeric between 0 and 1")
+    if (!all(is.numeric(rho), rho > 0 & rho <= 1))
+        stop("rho must be numeric > 0 and <= 1")
+    if (!all(is.numeric(M) | is.integer(M), M >= 0))
+        stop("Sample size (M) must be integer or numeric greater than 0")
+    if (!all(is.numeric(R), R > 0))
+        stop("Reproductive number (R) must be numeric greater than 0")
+    if (!all(is.numeric(R), R <= 1))
+        warning("Reproductive number (R) is usually less than 1 for finite outbreaks")
+
+    (M * rho * (R + 1) * eta)/2
+}
---
+++ R/truediscoveryrate.R
@@ -0,0 +1,60 @@
+##' Calculate true discovery rate of a sample
+##'
+##' This function calculates the true discovery rate (proportion of true transmission pairs) in a sample given the sensitivity \eqn{\eta}
+##' and specificity \eqn{\chi} of the linkage criteria, and sample size \eqn{M}. Assumptions about transmission and linkage (single or multiple)
+##' can be specified.
+##'
+##' @param eta scalar or vector giving the sensitivity of the linkage criteria
+##' @param chi scalar or vector giving the specificity of the linkage criteria
+##' @param rho scalar or vector giving the proportion of the final outbreak size that is sampled
+##' @param M scalar or vector giving the number of cases sampled
+##' @param R scalar or vector giving the effective reproductive number of the pathogen (default=NULL)
+##' @param assumption a character vector indicating which assumptions about transmission and linkage criteria. Default = \code{'mtml'}. Accepted arguments are:
+##' \enumerate{
+##'      \item \code{'stsl'} for the single-transmission single-linkage assumption (\code{\link{prob_trans_stsl}}).
+##'      \item \code{'mtsl'} for the multiple-transmission single-linkage assumption (\code{\link{prob_trans_mtsl}}).
+##'      \item \code{'mtml'} for the multiple-transmission multiple-linkage assumption (\code{\link{prob_trans_mtml}}).
+##'      }
+##'
+##' @return scalar or vector giving the true discovery rate
+##'
+##' @author John Giles, Shirlee Wohl, and Justin Lessler
+##'
+##' @examples
+##' # The simplest case: single-transmission, single-linkage, and perfect sensitivity
+##' truediscoveryrate(eta=1, chi=0.9, rho=0.5, M=100, assumption='stsl')
+##'
+##' # Multiple-transmission and imperfect sensitivity
+##' truediscoveryrate(eta=0.99, chi=0.9, rho=1, M=50, R=1, assumption='mtsl')
+##'
+##' # Small outbreak, larger sampling proportion
+##' truediscoveryrate(eta=0.99, chi=0.95, rho=1, M=50, R=1, assumption='mtml')
+##'
+##' # Large outbreak, small sampling proportion
+##' truediscoveryrate(eta=0.99, chi=0.95, rho=0.5, M=1000, R=1, assumption='mtml')
+##'
+##' @family discovery_rate
+##'
+##' @export
+##'
+
+truediscoveryrate <- function(eta, chi, rho, M, R = NULL, assumption = "mtml") {
+    lifecyle::deprecate_soft("1.0.0", "truediscoveryrate()", "translink_tdr()")
+    lifecyle::deprecate_soft("1.0.0", "truediscoveryrate(eta)", "translink_tdr(sensitivity)")
+    lifecyle::deprecate_soft("1.0.0", "truediscoveryrate(chi)", "translink_tdr(specificity)")
+
+    if (assumption == "stsl") {
+        message("Calculating true discovery rate assuming single-transmission and single-linkage")
+        out <- prob_trans_stsl(eta = eta, chi = chi, rho = rho, M = M)
+    } else if (assumption == "mtsl") {
+        message("Calculating true discovery rate assuming multiple-transmission and single-linkage")
+        out <- prob_trans_mtsl(eta = eta, chi = chi, rho = rho, M = M, R = R)
+    } else if (assumption == "mtml") {
+        message("Calculating true discovery rate assuming multiple-transmission and multiple-linkage")
+        out <- prob_trans_mtml(eta = eta, chi = chi, rho = rho, M = M, R = R)
+    } else {
+        stop("Incorrect assumption argument")
+    }
+
+    return(out)
+}
