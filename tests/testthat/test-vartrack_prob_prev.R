bdfc0b2c01fbdaaa6763d0cb3f841e1eb4f3d46c
Elizabeth Lee <eclee25@gmail.com>
2023-02-08 14:12:32 -0800

    rm unit tests related to external error handling

--- tests/testthat/test-vartrack_prob_prev.R
+++ tests/testthat/test-vartrack_prob_prev.R
@@ -2,8 +2,6 @@ test_that("vartrack_prob_prev input arguments are valid", {
 
     expect_error(vartrack_prob_prev(p_v1 = 0.1, n = 30, omega = 0.9, c_ratio = 1.5, sampling_freq = "fake"),
         "sampling frequency argument")
-    expect_error(vartrack_prob_prev(n = 30, c_ratio = 1, sampling_freq = "xsect"),
-        "missing")
     expect_error(vartrack_prob_prev(p_v1 = 0.1, n = 30, omega = 0.9, c_ratio = 1.5, sampling_freq = "cont"),
         "not yet implemented")
 })
--- tests/testthat/test-vartrack_samplesize_prev.R
+++ tests/testthat/test-vartrack_samplesize_prev.R
@@ -2,8 +2,6 @@ test_that("vartrack_samplesize_prev input arguments are valid", {
 
     expect_error(vartrack_samplesize_prev(p_v1 = 0.1, prob = 0.9, precision = 0.1, omega = 0.9, c_ratio = 1.5, sampling_freq = "fake"),
         "sampling frequency argument")
-    expect_error(vartrack_samplesize_prev(p_v1 = 0.2, c_ratio = 1, sampling_freq = "xsect"),
-        "missing")
     expect_error(vartrack_samplesize_prev(p_v1 = 0.1, omega = 0.9, c_ratio = 1.5, sampling_freq = "cont"),
         "not yet implemented")
 })
